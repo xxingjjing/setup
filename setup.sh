155f398a64589094f7ca1a9f909f91009baaba34
sssa <313999717@qq.com>
2020-08-25 16:14:08 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -54,17 +54,8 @@ create_config() {
 }
 
 setup_compose() {
-    REQUESTED_CHANNEL=stable
-    LATEST_VERSION=`curl -s "https://version.redash.io/api/releases?channel=$REQUESTED_CHANNEL"  | json_pp  | grep "docker_image" | head -n 1 | awk 'BEGIN{FS=":"}{print $3}' | awk 'BEGIN{FS="\""}{print $1}'`
-
     cd $REDASH_BASE_PATH
-    GIT_BRANCH="${REDASH_BRANCH:-master}" # Default branch/version to master if not specified in REDASH_BRANCH env var
-    wget https://github.com/xxingjjing/setup/edit/master/data/docker-compose.yml
-    sed -ri "s/image: redash\/redash:([A-Za-z0-9.-]*)/image: redash\/redash:$LATEST_VERSION/" docker-compose.yml
-    echo "export COMPOSE_PROJECT_NAME=redash" >> ~/.profile
-    echo "export COMPOSE_FILE=/opt/redash/docker-compose.yml" >> ~/.profile
     export COMPOSE_PROJECT_NAME=redash
-    export COMPOSE_FILE=/opt/redash/docker-compose.yml
     sudo docker-compose run --rm server create_db
     sudo docker-compose up -d
 }
